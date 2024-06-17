c675eeccebe52a8914c9f2e95048f89371401138
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-06-17 17:38:37 +0530

    add env variables

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -17,7 +17,7 @@ const handler = NextAuth({
       return session;
     }
   },
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
