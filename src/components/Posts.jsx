a846216f51d1630eee3c95d8845c12ed7f6d8f3f
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-07-26 18:58:54 +0530

    update the Posts component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import {app} from '../firebase'
-import { collection, getDocs, getFirestore, orderBy, query } from 'firebase/firestore'
-import Post from './Post';
+'use client'
+import React, { useEffect, useState } from "react";
+import { app } from "../firebase";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import Post from "./Post";
 
+export default function Posts() {
+  const [data, setData] = useState([])
+  const fetchData = async () => {
+    const db = getFirestore(app);
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    const querySnapshot = await getDocs(q);
+    let datas = [];
+    querySnapshot.forEach((doc) => {
+      datas.push({ id: doc.id, ...doc.data() });
+    });
+    setData(datas)
+  };
 
-export const dynamic = 'force-dynamic';
-export default async function Posts() {
-  const db = getFirestore(app);
-  const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
-  const querySnapshot = await getDocs(q);
-  let data = [];
-  querySnapshot.forEach((doc) => {
-    data.push({id: doc.id, ...doc.data()});
-  })
+  useEffect(()=> {
+    fetchData()
+  },[data, fetchData])
+
   return (
-    <div>
-      {
-        data && data.map((post)=>(
-          <Post key={post.id} post={post}/>
-        ))
-      }
-    </div>
-  )
+    <div>{data && data.map((post) => <Post key={post.id} post={post} />)}</div>
+  );
 }
-
