dd4b9eb2ee84a606fd73a04423b590548437ef81
Siva-S92 <sivagraphics4fashion@gmail.com>
2025-07-30 22:31:50 +0530

    finalize it

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import Posts from './Posts'
-import MiniProfile from './MiniProfile'
+"use client";
+import React, { useEffect, useState } from "react";
+import Posts from "./Posts";
+import MiniProfile from "./MiniProfile";
 //after a user signs in with Google (via NextAuth), Firebase recognizes the user as authenticated.
 import { useSession } from "next-auth/react";
 import { getAuth, signInWithCustomToken } from "firebase/auth";
@@ -9,7 +9,7 @@ import { getAuth, signInWithCustomToken } from "firebase/auth";
 export default function Feed() {
   //after a user signs in with Google (via NextAuth), Firebase recognizes the user as authenticated.
   const { data: session, status } = useSession();
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const authenticateWithFirebase = async () => {
@@ -29,8 +29,10 @@ export default function Feed() {
           // Sign in with the custom token
           const auth = getAuth();
           await signInWithCustomToken(auth, customToken);
-          console.log("Successfully signed in with Firebase using custom token");
-          setIsAuthenticated(true)
+          console.log(
+            "Successfully signed in with Firebase using custom token"
+          );
+          setIsAuthenticated(true);
         } catch (error) {
           console.error("Error signing in with Firebase:", error);
         }
@@ -44,21 +46,19 @@ export default function Feed() {
 
   if (status === "loading") return <p>Loading...</p>;
 
-  //*****************************************************************************************************
   return (
-    <main className='grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto'>
-        {/* posts left */}
-        <section className='md:col-span-2'>
-            <Posts/>
-        </section>
+    <main className="grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto">
+      {/* posts left */}
+      <section className="md:col-span-2">
+        <Posts />
+      </section>
 
-
-        {/* Mini profile rightside */}
-        <section className='hidden md:inline-grid md:col-span-1'>
-            <div className='fixed w-[380px]'>
-             <MiniProfile/>
-            </div>
-        </section>
+      {/* Mini profile rightside */}
+      <section className="hidden md:inline-grid md:col-span-1">
+        <div className="fixed w-[380px]">
+          <MiniProfile />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
